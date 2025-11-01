f6753e6f341e74f54c7c1b725ddf4d4a9f65b1f6
Andrés Oswaldo <oswaldocobos5@gmail.com>
2025-10-31 20:45:31 -0600

    Feature: Creación de la página de Perfil de Usuario.

---
+++ vistas/Usuario/PerfilUsuarioPage.xaml.cs
@@ -0,0 +1,22 @@
+namespace pasadena_vistas.vistas.Usuario;
+
+public partial class PerfilUsuarioPage : ContentPage
+{
+	public PerfilUsuarioPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void EditarPerfil_Tapped(object sender, TappedEventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(EditarPerfilPage));
+    }
+    private async void Social_Tapped(object sender, TappedEventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(SocialPage));
+    }
+    private async void AdminPanel_Tapped(object sender, TappedEventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(vistas.Administrador.GestionarUsuariosPage));
+    }
+}
