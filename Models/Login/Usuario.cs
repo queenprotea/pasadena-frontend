d297b147743cb9d8a57ddede1fbb1ad33ce2f71a
x3776x <abrahamvvquinto@gmail.com>
2025-11-27 23:38:57 -0600

    add simple login logic. Yet to add tests and validations.

---
+++ Models/Login/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace pasadena_vistas.Models.Login
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string full_name { get; set; }
+        public string username { get; set; }
+        public bool is_active { get; set; }
+        public int role_id { get; set; }
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using pasadena_vistas.Config;
+using pasadena_vistas.Models.Login;
+using System.Net.Http.Json;
+namespace pasadena_vistas.Services;
+
+public class AuthService
+{
+    private readonly HttpClient _clienteHttp;
+    private string urlBase = Config.Config.AuthLogin;
+
+    public AuthService()
+    {
+        _clienteHttp = new HttpClient();
+    }
+
+    public async Task<TokenRespuesta> LoginAsync(string identifier, string password)
+    {
+        var loginInfo = new SolicitudLogin
+        {
+            identifier = identifier,
+            password = password
+        };
+
+        var respuesta = await _clienteHttp.PostAsJsonAsync($"{urlBase}", loginInfo);
+
+        if (!respuesta.IsSuccessStatusCode)
+            throw new Exception("Credenciales incorrectas");
+
+        return await respuesta.Content.ReadFromJsonAsync<TokenRespuesta>();
+    }
+}
